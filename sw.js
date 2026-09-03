6d8293d21c97d86d4517e9aa7429b845dbb67a2e
dlpburgers-jpg <dlpburgers@gmail.com>
2026-09-03 20:20:03 -0300

    Publicar cabecera sin damero

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* global __ASSETS__ */
-const CACHE = 'dlp-v5-89c5e763023d';
-const FILES = ["./index.html","./manifest.webmanifest","./favicon.svg","./icon-192.png","./icon-512.png","./assets/dlp-burgers-banner-D8DG3Hn2.png","./assets/dlp-campaign-S8xeskhn.jpeg","./assets/index-BpI6Vt6T.css","./assets/index-Cu6bqkiv.js","./assets/preview-disabled-CwRR3fzr.js"];
+const CACHE = 'dlp-v5-ae17cd571eec';
+const FILES = ["./index.html","./manifest.webmanifest","./favicon.svg","./icon-192.png","./icon-512.png","./assets/dlp-burgers-banner-D8DG3Hn2.png","./assets/dlp-campaign-S8xeskhn.jpeg","./assets/index-BuoOWp3L.css","./assets/index-D5jTqK3-.js","./assets/preview-disabled-CwRR3fzr.js"];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('dlp-v5-') && key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())); });
 self.addEventListener('message',event=>{
