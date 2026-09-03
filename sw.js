1ca405327b77207c257bb744b2535c6a5861d474
dlpburgers-jpg <dlpburgers@gmail.com>
2026-09-02 23:42:40 -0300

    Habilitar cupones sobre el carrito y pagos por entrega

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'dlp-v5-2483fda4258c';
-const FILES = ["./index.html","./manifest.webmanifest","./favicon.svg","./icon-192.png","./icon-512.png","./assets/index-BYChdS8-.js","./assets/index-DmgGA1_Q.css","./assets/preview-disabled-CwRR3fzr.js"];
+const CACHE = 'dlp-v5-34c69324bd86';
+const FILES = ["./index.html","./manifest.webmanifest","./favicon.svg","./icon-192.png","./icon-512.png","./assets/index-Bd6BZj1T.css","./assets/index-Cqm6fVeg.js","./assets/preview-disabled-CwRR3fzr.js"];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('dlp-v5-') && key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', event => {
