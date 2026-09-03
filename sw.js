f03952b4b0970212c620c83799f5e8561a4be15e
dlpburgers-jpg <dlpburgers@gmail.com>
2026-09-03 19:13:01 -0300

    Publicar salón con comensales y acceso de impresión

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* global __ASSETS__ */
-const CACHE = 'dlp-v5-bfe427eeb4f1';
-const FILES = ["./index.html","./manifest.webmanifest","./favicon.svg","./icon-192.png","./icon-512.png","./assets/dlp-campaign-S8xeskhn.jpeg","./assets/index-BUm2ohh_.js","./assets/index-Dv6QoA0L.css","./assets/preview-disabled-CwRR3fzr.js"];
+const CACHE = 'dlp-v5-b0138904e884';
+const FILES = ["./index.html","./manifest.webmanifest","./favicon.svg","./icon-192.png","./icon-512.png","./assets/dlp-campaign-S8xeskhn.jpeg","./assets/index-BJF836CF.js","./assets/index-CTc8zuqA.css","./assets/preview-disabled-CwRR3fzr.js"];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('dlp-v5-') && key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())); });
 self.addEventListener('message',event=>{
