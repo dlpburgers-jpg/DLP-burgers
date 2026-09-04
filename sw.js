16cf0cce8dc292c9b09b006b4686fe47cc16f474
dlpburgers-jpg <dlpburgers@gmail.com>
2026-09-03 22:07:09 -0300

    Publicar fondo rojo liso sin trama

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* global __ASSETS__ */
-const CACHE = 'dlp-v5-a33b8d625b2d';
-const FILES = ["./index.html","./manifest.webmanifest","./favicon.svg","./icon-192.png","./icon-512.png","./assets/dlp-admin-logo-CKBb41bS.jpeg","./assets/dlp-burgers-banner-D8DG3Hn2.png","./assets/dlp-campaign-S8xeskhn.jpeg","./assets/index-CetjnBOn.css","./assets/index-CrpCSgOl.js","./assets/preview-disabled-CwRR3fzr.js"];
+const CACHE = 'dlp-v5-5ad4cf40e40d';
+const FILES = ["./index.html","./manifest.webmanifest","./favicon.svg","./icon-192.png","./icon-512.png","./assets/dlp-admin-logo-CKBb41bS.jpeg","./assets/dlp-burgers-banner-D8DG3Hn2.png","./assets/dlp-campaign-S8xeskhn.jpeg","./assets/index-Cd_BCIE1.css","./assets/index-uZHv_Vpt.js","./assets/preview-disabled-CwRR3fzr.js"];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('dlp-v5-') && key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())); });
 self.addEventListener('message',event=>{
